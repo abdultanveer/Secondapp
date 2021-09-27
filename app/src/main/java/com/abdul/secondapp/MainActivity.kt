38985cc7d1abf59c61373e40df83e765d751d9fd
abdul <abdul.tanveer@gmail.com>
2021-09-27 19:27:29 +0530

    EditTextWithClear.java

--- app/src/main/java/com/abdul/secondapp/MainActivity.kt
+++ app/src/main/java/com/abdul/secondapp/MainActivity.kt
@@ -11,6 +11,7 @@ import android.widget.SimpleCursorAdapter
 import android.content.pm.PackageManager
 
 import android.os.Build
+import android.os.PersistableBundle
 import android.provider.CallLog
 import android.util.Log
 import android.view.View
@@ -58,6 +59,11 @@ class MainActivity : AppCompatActivity() {
 
 
     }
+
+    override fun onSaveInstanceState(outState: Bundle, outPersistentState: PersistableBundle) {
+        super.onSaveInstanceState(outState, outPersistentState)
+    }
+
 
     private fun setupListView() {
         //var listview: ListView = findViewById(R.id.cpList)
